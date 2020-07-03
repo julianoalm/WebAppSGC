a974ee78709adc209aef1319947800bd0b4e598c
Juliano Martins <julianoalm@gmail.com>
2020-07-03 09:50:27 -0300

    First Commit

---
+++ src/SGC.Infrastructure/Data/ClienteContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.ApplicationCore.Entity;
+using SGC.Infrastructure.EntityConfig;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Infrastructure.Data
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+            modelBuilder.Entity<Contato>().ToTable("Contato");
+            modelBuilder.Entity<Endereco>().ToTable("Endereco");
+            modelBuilder.Entity<Profissao>().ToTable("Profissao");
+            modelBuilder.Entity<ProfissaoCliente>().ToTable("ProfissaoCliente");
+            modelBuilder.ApplyConfiguration(new ClienteMap());
+            modelBuilder.ApplyConfiguration(new ContatoMap());
+            modelBuilder.ApplyConfiguration(new ProfissaoMap());
+            modelBuilder.ApplyConfiguration(new EnderecoMap());
+            modelBuilder.ApplyConfiguration(new ProfissaoClienteMap());
+            modelBuilder.ApplyConfiguration(new MenuMap());
+
+        }
+    }
+}
---
+++ src/SGC.Infrastructure/Repository/ClienteRepository.cs
@@ -0,0 +1,35 @@
+﻿using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Interfaces.Repository;
+using SGC.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SGC.Infrastructure.Repository
+{
+    public class ClienteRepository : EFRepository<Cliente>, IClienteRepository
+    {
+
+        public ClienteRepository(ClienteContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public override Cliente Adicionar(Cliente entity)
+        {
+            var verificarResutado = "";
+            //TODO implementação específica para adicionar um cliente fazendo a sobrescrita de método - polimorfismo
+            _dbContext.Set<Cliente>().Add(entity);
+            _dbContext.SaveChanges();
+
+            return entity;
+        }
+
+        public Cliente ObterPorProfissao(int clienteId)
+        {
+            return Buscar(x=> x.ProfissoesClientes.Any(p=> p.ClienteId == clienteId))
+                .FirstOrDefault();
+        }
+    }
+}
